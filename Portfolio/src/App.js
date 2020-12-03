6ef5393d34e66039625f4bcaeaa1f5fdd1d081a0
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-12-03 12:05:18 -0600

    changed front end
    initially used create-js-app changed to create-react-app

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { value, onIncrement, onDecrement } = this.props;
-    return (
-      <div className="App">
-      <h1>A React app</h1>
-      <p>
-      Clicked: {value} times
-      {' '}
-      <button onClick={onIncrement}>
-      +
-      </button>
-      {' '}
-      <button onClick={onDecrement}>
-      -
-      </button></p>
-      </div>
-      );
-    }
-  }
-  
-  export default App;
-  
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- Portfolio/src/index.js
+++ Portfolio/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import './index.css';
 import App from './App';
-import counter from './counter';
+import reportWebVitals from './reportWebVitals';
 
-const store = createStore(counter);
-
-const render = () => ReactDOM.render(
-  <App
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-render();
-store.subscribe(render);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
