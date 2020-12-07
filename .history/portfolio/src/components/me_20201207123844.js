86fd3f8977ea2863705606407467dba58d491826
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2020-12-07 13:08:23 -0600

    added some css stuff
    back ground collor and emphasis also started a todo list in the file for my portfolio and this project.

---
+++ .history/portfolio/src/components/me_20201207123844.js
@@ -0,0 +1,24 @@
+import { Component  } from "react";
+import portfolioPic from "./images/summer2020.jpg";
+
+class Me extends Component{
+    state = {
+        name: "Charles Pelton",
+        moto: "Continually work on the person in the mirror, in order to change the world.",
+        career: "Operations Supervisor, DHL Supply Chain",
+        aspiration: "Full stack web developer / Software Engineer"
+    }
+    render(){
+        return(
+            <div id="me">
+                {this.state.name}<br />
+                <img src={portfolioPic} alt='Me' width='250' height='375' /><br />
+                <em>{this.state.moto}</em><br />
+                <b>{this.state.career}</b><br />
+                <b><em>{this.state.aspiration}</em></b>
+            </div>
+        )
+    }
+};
+
+export default Me;
